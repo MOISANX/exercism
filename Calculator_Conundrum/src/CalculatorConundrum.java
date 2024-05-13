619a3c84270368c4b2072a3383d7b005f3d1825f
Moises Santos <moisessantosddt@gmail.com>
2024-05-13 15:02:27 -0300

    atualizando o calaculator

--- Calculator_Conundrum/src/CalculatorConundrum.java
+++ Calculator_Conundrum/src/CalculatorConundrum.java
@@ -1,13 +1,45 @@
 public class CalculatorConundrum {
 
   public String calculate(int operand1, int operand2, String operation) {
-     int result = 0;
-    if (operation.equals("+")) {
-       result = operand1 + operand2;
-
-      }
-      
-      return String.valueOf(result);
-     
+    if(operation == null) {
+      throw new IllegalArgumentException("Operation cannot be null");
     }
+    if(operation.isEmpty()){
+      throw new IllegalArgumentException("Operation cannot be empty");
+    }
+
+    switch (operation) {
+      case "+":
+          return operand1 + " + " + operand2 + " = " + (operand1 + operand2);
+      
+          case "*":
+          return operand1 + " * " + operand2 + " = " +  (operand1 * operand2);
+
+          case "/":
+             try {
+                 if(operand2 == 0){
+                   throw new ArithmeticException("Division by zero is not allowed");
+                 }
+                 return operand1 + " / " + operand2 + " = " + (operand1 / operand2);
+             } catch (ArithmeticException e){
+              throw new IllegalArgumentException ("Division by zero is not allowed", e);
+             }
+        
+    
+      default:
+      throw new IllegalArgumentException("Operation '"+ operation +"' does not exist");
+      
+    }
+
+    
+     
+ }
+
+ public static void main(String[] args) {
+     CalculatorConundrum calculatorConundrum =  new CalculatorConundrum();
+
+    System.out.println(calculatorConundrum.calculate(10, 5, "/")); 
+
+
+ }
 }
